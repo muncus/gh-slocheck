4a483b6844a374b9350670081caf6de9880c2024
Marc Dougherty <muncus@users.noreply.github.com>
2022-11-02 18:23:29 -0700

    feat: allow multiple searches, and de-dupe the results.
    This restricts output sort order to oldest-first.

--- search/pullrequest.go
+++ search/pullrequest.go
@@ -2,11 +2,37 @@ package search
 
 import (
 	"fmt"
+	"sort"
 	"time"
 
 	"github.com/cli/go-gh"
 	graphql "github.com/cli/shurcooL-graphql"
 )
+
+// PRSet de-dupes PRInfos by URL, allowing for the merging of multiple lists
+type PRSet map[string]PRInfo
+
+func (ps PRSet) Add(pr PRInfo) {
+	ps[pr.URL] = pr
+}
+func (ps PRSet) Remove(pr PRInfo) {
+	if _, ok := ps[pr.URL]; ok {
+		delete(ps, pr.URL)
+	}
+}
+func (ps PRSet) Merge(a []PRInfo) {
+	for _, p := range a {
+		ps.Add(p)
+	}
+}
+func (ps PRSet) ByLastUpdate() []PRInfo {
+	s := make([]PRInfo, 0)
+	for _, pr := range ps {
+		s = append(s, pr)
+	}
+	sort.SliceStable(s, func(i, j int) bool { return s[i].UpdatedAt.Before(s[j].UpdatedAt) })
+	return s
+}
 
 // a subset of information about a Pull Request.
 // see https://docs.github.com/en/graphql/reference/objects#pullrequest
@@ -18,7 +44,8 @@ type PRInfo struct {
 	Mergeable      string
 	ReviewDecision string
 	BaseRepository struct {
-		Name string
+		Name          string
+		NameWithOwner string
 	}
 	Commits   Commits `graphql:"commits(last: 1)"`
 	UpdatedAt time.Time
@@ -38,7 +65,7 @@ func (p PRInfo) SinceLastUpdate() time.Duration {
 	return time.Since(p.UpdatedAt)
 }
 
-func SearchPRs(q string, vars map[string]interface{}) ([]PRInfo, error) {
+func SearchPRs(q string, vars map[string]interface{}) (PRSet, error) {
 	gqClient, err := gh.GQLClient(nil)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to create GraphQL client: %v", err)
@@ -56,9 +83,9 @@ func SearchPRs(q string, vars map[string]interface{}) ([]PRInfo, error) {
 		return nil, err
 	}
 	// fmt.Printf("Query results: %#v\n", query)
-	prinfos := make([]PRInfo, 0)
+	prlist := PRSet{}
 	for _, n := range query.Search.Nodes {
-		prinfos = append(prinfos, n.PullRequest)
+		prlist.Add(n.PullRequest)
 	}
-	return prinfos, nil
+	return prlist, nil
 }
